807786a3c97738fd471dcef6cb97b0dfe1a6efa9
Luis Casillas <luiscasillas@n12.mx>
2018-10-04 17:27:12 -0500

    get added

--- api/private/quote.js
+++ api/private/quote.js
@@ -7,28 +7,36 @@ const app = express.Router();
 
 app.group('/quotes', (router) => {
     router
+        .get('/',(req,res)=>{
+          quoteCtrl
+            .listAll()
+            .then(quoteList => {
+                res.status(200).json(quoteList);
+            })
+            .catch(err => {
+                res.status(200).json({ error: err });
+            });
+
+        })
         .post('/',
         (req, res) => {
           const {body} = req
           quoteCtrl.addOne(req.body)
-                .then(quote => {
-                    if (quote.hasOwnProperty('error'))
-                        res.status(200).json({
-                            message: quote.message,
-                            error: quote.error
-                        });
-                    else {
-                        res.status(201).json({
-                            message: 'Cotizacion agregada satisfactoriamente',
-                        });
-                    }
-                })
-                .catch(err => {
-                    res.status(200).json({ err: err });
-                });
-
-        })
-        .get('/',(req,res)=>{
+          .then(quote => {
+            if (quote.hasOwnProperty('error'))
+            res.status(200).json({
+              message: quote.message,
+              error: quote.error
+            });
+            else {
+              res.status(201).json({
+                message: 'Cotizacion agregada satisfactoriamente',
+              });
+            }
+          })
+          .catch(err => {
+            res.status(200).json({ err: err });
+          });
 
         })
 
