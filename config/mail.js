18e09641db24926bcb2f3e2e57200d0a1ba18cb9
Luis Casillas <luiscasillas@n12.mx>
2018-10-10 11:12:10 -0500

    intro and template change

--- config/mail.js
+++ config/mail.js
@@ -29,7 +29,7 @@ const transporter = nodemailer.createTransport(
         debug:true,
         auth: {
             user: 'alo@workosfera.com',
-            pass: '@Workosfera123'
+            pass: '@Cowork567'
         },
         tls: {
         // do not fail on invalid certs
