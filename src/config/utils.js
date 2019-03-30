abdeaa4e0332d8bdcc1a656697434fae4203865a
Luis Casillas <luiscasillas@n12.mx>
2019-03-29 20:18:46 -0600

    Cors protection

--- src/config/utils.js
+++ src/config/utils.js
@@ -11,10 +11,30 @@ if(process.env.NODE_ENV===undefined){
 /**
  * Function to enable cors, logger, disable x-powered-by, urlencoded
  */
+let whiteList = []
+if(process.env.NODE_ENV==='development'){
+  whiteList = ['http://quoter.workosfera.com', 'http://localhost:3000']
+  // whiteList = ['http://quoter.workosfera.com']
+} else {
+  whiteList = ['http://quoter.workosfera.com']
+}
+
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
 export const utils = (app) => {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use(helmet())
   if(process.env.NODE_ENV==='development'){
     app.use(logger('dev'))
